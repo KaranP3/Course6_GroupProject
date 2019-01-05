a9aad76121c4567673500990906c9dc94763ef48
mukul <mnshrt2292@gmail.com>
2019-01-06 00:17:07 +0530

    Like button click event implemented

--- stub/js/post.js
+++ stub/js/post.js
@@ -2,6 +2,9 @@ var blogTitle= document.getElementById("blogTitle");
 var blogBody= document.getElementById("blogBody");
 var blogEditArea = document.getElementById("blogEditArea");
 var saveEditButton = document.getElementById("editContentButton");
+var likeButton = document.getElementById("likeButton");
+var likeText= document.getElementById("likeText");
+var likeCounter =0;
 
 
 //click event for editContentButton
@@ -9,16 +12,16 @@ saveEditButton.addEventListener("click",()=>{
 	var blogContent='';
 	if(saveEditButton.innerHTML.includes('Edit')){
 		
-		  saveEditButton.innerHTML='Save &nbsp <i class="fa fa-save"></i>';
-          blogContent=blogBody.innerHTML.trim();
-          console.log(blogContent);
-		  blogBody.style.display="none";
-		  blogTitle.setAttribute("contenteditable",true);
-		  blogTitle.style.border= "thin solid #F02875";
-		  blogEditArea.style.display="block";
-	      blogEditArea.innerHTML=blogContent;
-		  blogEditArea.focus();
-		  blogEditArea.setSelectionRange(0, 0);
+		saveEditButton.innerHTML='Save &nbsp <i class="fa fa-save"></i>';
+		blogContent=blogBody.innerHTML.trim();
+		console.log(blogContent);
+		blogBody.style.display="none";
+		blogTitle.setAttribute("contenteditable",true);
+		blogTitle.style.border= "thin solid #F02875";
+		blogEditArea.style.display="block";
+		blogEditArea.innerHTML=blogContent;
+		blogEditArea.focus();
+		blogEditArea.setSelectionRange(0, 0);
 
 
 
@@ -34,3 +37,16 @@ saveEditButton.addEventListener("click",()=>{
 	}
 
 });
+
+
+// on likeButton click
+likeButton.addEventListener('click',()=>{
+	likeCounter++;
+	likeButton.innerHTML="Liked!";
+	if(likeCounter==1){
+		likeText.innerHTML=likeCounter + " person has liked this!";
+	}else{
+		likeText.innerHTML= likeCounter+" people have liked this!";
+	}
+
+})
