696c38a79f2d8ad246d76dba9fc59c7160cb0190
kritijain2110 <43193916+kritijain2110@users.noreply.github.com>
2019-01-06 12:08:34 +0530

    Added SignUp modal. Files modified: stub/js/header.js stub/index.htm stub/js/header.js

--- stub/js/header.js
+++ stub/js/header.js
@@ -10,3 +10,15 @@ function close_signIn(){
     SignIn.style.display = "none";
 
 }
+
+function signUp(){
+    SignIn.style.display = "none";
+    CreatePost.style.display = "none";
+    SignUp.style.display = "block";
+
+}
+function close_signUp(){
+
+    SignUp.style.display = "none";
+
+}
